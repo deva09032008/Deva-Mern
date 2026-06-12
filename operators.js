73af4cce3724268382467e1ccb5e78e865bb20e6
deva09032008 <devadharshinik25csa@vetias.ac.in>
2026-06-12 14:04:55 +0530

    Added ternary

--- operators.js
+++ operators.js
@@ -8,6 +8,7 @@ console.log(a*b)//multiplication
 console.log(a/b)//divide
 console.log(a%b)//modulus
 console.log(a**b)//expo
+
 //Assignment operator
 console.log("Assignment Operator")
 var x=10
@@ -15,6 +16,7 @@ console.log(x+=20)
 console.log(x-=20)
 console.log(x*=20)
 console.log(x/=20) 
+
 //Comparison Operator
 console.log("Comparison Operator")
 var x=10
@@ -25,17 +27,20 @@ console.log(x<=y)
 console.log(x>=y)
 console.log(x<y)
 console.log(x>y)
+
 //Logical operators
 console.log("Logical Operators")
 console.log(a > 0 && b > 0)// o/p-true (both statements should be true) 
 console.log(a > 0 || b < a)// o/p-true (Any one statement can be true)
 console.log(!(a > 0))//o/p- false (opposite to the statement)
+
 //unary operation
 console.log("Unary Operator")
 console.log(x++)
 console.log(x--)
 console.log(++x)
 console.log(--x)
+
 //ternary operation
 console.log("Ternary Operation")
 var result = ( a > b) ? "a is greater" : "b is greater"

d185d8821967ecaf95c6b9bcd2513d1fe285c886
deva09032008 <devadharshinik25csa@vetias.ac.in>
2026-06-12 10:32:42 +0530

    Added variable.js

---
+++ variable.js
@@ -0,0 +1,9 @@
+//variable declaration using var
+var a=10
+console.log(a)
+//variable declaration using let
+let name='DEVA'
+console.log(name)
+//variable declaration using const
+const college='VETIAS'
+console.log(college)

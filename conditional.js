e27aee9a2e655dc85f81ddad96489a604f7e296e
deva09032008 <devadharshinik25csa@vetias.ac.in>
2026-06-12 15:49:23 +0530

    Added conditional

---
+++ conditional.js
@@ -0,0 +1,12 @@
+var light="white"
+
+if(light=="green"){
+    console.log("Go!")
+}
+else if(light=="yellow"){
+    console.log("Wait")
+}
+else{
+    console.log("Invalid light color")
+}
+console.log("Outside If")

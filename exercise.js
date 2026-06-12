70169eb390e7e96970b7ba2d5fa8dc70e9870447
deva09032008 <devadharshinik25csa@vetias.ac.in>
2026-06-12 16:10:00 +0530

    Added exercise.js

--- exercise.js
+++ exercise.js
@@ -1,1 +1,10 @@
 //function
+var factor="Vikram"
+var fplayer="M.S.Dhoni"
+var fmovie="24"
+function favorite(){
+    console.log("favorite actor :"+factor)
+    console.log("favorite player :"+fplayer)
+    console.log("favorite movie :"+fmovie)
+}
+favorite()

a0fd658f29bcae9081f6dbe159a7adb48bb97040
deva09032008 <devadharshinik25csa@vetias.ac.in>
2026-06-12 14:55:43 +0530

    Added datatypes

---
+++ datatypes.js
@@ -0,0 +1,39 @@
+//Data types
+console.log("Primitive Datatype")
+
+//Numbers
+console.log("DT-Numbers")
+var age=20
+console.log(age)
+
+//string
+console.log("DT-String")
+var name="DEVA"
+console.log(name)
+
+//boolean
+console.log("DT-Boolean")
+var isStudent=true
+console.log(isStudent)
+
+//null
+console.log("DT-Null")
+var emptyelement=null
+
+
+console.log(emptyelement)
+
+//undefined
+console.log("DT-Undefined")
+var Undefined
+console.log(undefined)
+
+//bigint
+console.log("DT-BigInt")
+var big=1234567890987654322
+console.log(big)
+
+//symbol
+console.log("DT-Symbol")
+var symbolvalue="Symbol"
+console.log(symbolvalue)

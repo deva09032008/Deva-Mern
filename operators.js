d151bfb6d1778ebe8847f3188df123b527036f90
deva09032008 <devadharshinik25csa@vetias.ac.in>
2026-06-12 10:59:14 +0530

    Added operator

---
+++ operators.js
@@ -0,0 +1,9 @@
+//Arithmetic operator
+var a=10
+var b=15
+console.log(a+b)//add
+console.log(a-b)//subtract
+console.log(a*b)//multiplication
+console.log(a/b)//divide
+console.log(a%b)//modulus
+console.log(a**b)//expo

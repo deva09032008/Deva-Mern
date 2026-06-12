b4044cbeb03ca245c988f7b3f09eb9bb62214d2a
deva09032008 <devadharshinik25csa@vetias.ac.in>
2026-06-12 11:18:47 +0530

    Added Assignment

--- operators.js
+++ operators.js
@@ -1,4 +1,5 @@
 //Arithmetic operator
+console.log("Arithmetic Operator")
 var a=10
 var b=15
 console.log(a+b)//add
@@ -7,3 +8,10 @@ console.log(a*b)//multiplication
 console.log(a/b)//divide
 console.log(a%b)//modulus
 console.log(a**b)//expo
+//Assignment operator
+console.log("Assignment Operator")
+var x=10
+console.log(x+=20)
+console.log(x-=20)
+console.log(x*=20)
+console.log(x/=20)

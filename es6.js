be1d95a521910d36c2b955e8da41985e2340cb0b
deva09032008 <devadharshinik25csa@vetias.ac.in>
2026-06-15 11:53:56 +0530

    Added es6.js

---
+++ es6.js
@@ -0,0 +1,49 @@
+let studentCount = 10;
+studentCount = 11;
+console.log("Student Count:", studentCount);
+
+//const
+const collegeName = "VETIAS";
+console.log("College Name:",collegeName);
+
+//Template literals
+let studentName = "Arun";
+console.log(`Welcome ${studentName} to ${collegeName}`);
+
+//object
+const  student = {
+    name:"Arun",
+    age:20,
+    department:"CSA"
+}
+console.log(student);
+//Destructuring
+const{ name, age, department } = student;
+console.log("Name:",name);
+console.log("Age:",age);
+console.log("Department:",department);
+
+//Arrow function
+const displayStudent = () => {
+    console.log(`Student Name: ${name}`);
+}
+//Array
+const  studentArray= ["Arun","priya","Deva"]
+console.log(studentArray);
+
+//spread operator
+
+const updatedStudent=[
+    ...studentArray,
+    "Nafila"
+]
+console.log(updatedStudent);
+//rest operator
+function maximumMarks(...marks){
+    let max = Math.max(...marks);
+    return max;
+}
+console.log("Maximum Marks:",maximumMarks(80,90,50));
+
+
+
